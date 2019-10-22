abf327e4bc705d11026b44e4058b658a7bb8d657
Tyler Bisig <bisig1892@gmail.com>
2019-10-22 14:47:52 -0700

    created functions for getting form info and displaying on page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,38 @@
+window.onload = function () {
+    var addBtn = document.querySelector('form > input[type=submit]');
+    addBtn.onclick = main;
+};
 /**
  * Represents a single task on a todo list
  */
 var ToDoItem = /** @class */ (function () {
-    function ToDoItem(title, deadline) {
-        this.title = title;
-        this.deadline = deadline;
-        this.isCompleted = false;
+    function ToDoItem(task) {
+        this.title = task;
     }
     return ToDoItem;
 }());
+function main() {
+    var item = getItem();
+    // Display new ToDoItem from page
+    displayToDoItem(item);
+    // Save ToDoItem
+}
+/**
+ * Gets the user input todo item from the form.
+ */
+function getItem() {
+    var title = document.getElementById('title').value;
+    var item = new ToDoItem(title);
+    var deadline = document.getElementById('deadline').value;
+    item.deadline = new Date(deadline);
+}
+/**
+ * Displays ToDoItem on the page
+ * @param item
+ */
+function displayToDoItem(item) {
+    var div = document.createElement('div');
+    div.innerHTML = '<input type="checkbox">' + item.title;
+    var displayDiv = document.getElementById('todo');
+    displayDiv.appendChild(div);
+}
