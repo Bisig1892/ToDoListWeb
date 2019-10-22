1485ae0af85e1f0755187f87c6ce64b44cef8a20
Tyler Bisig <bisig1892@gmail.com>
2019-10-22 14:42:28 -0700

    created functions for getting form info and displaying on page. displayToDoItem is currently not working

--- typescripts/main.ts
+++ typescripts/main.ts
@@ -1,3 +1,7 @@
+window.onload = function(){
+    let addBtn = <HTMLElement> document.querySelector('form > input[type=submit]')
+    addBtn.onclick = main;
+};
 /**
  * Represents a single task on a todo list
  */
@@ -6,9 +10,34 @@ class ToDoItem {
     deadline:Date;
     isCompleted:boolean;
     
-    constructor(title:string, deadline:Date) {
-        this.title = title;
-        this.deadline = deadline;
-        this.isCompleted = false;
+    constructor(task:string) {
+        this.title = task
     }
+}
+
+function main() {
+    let item:ToDoItem = getItem();
+    // Display new ToDoItem from page
+    displayToDoItem(item);
+    // Save ToDoItem
+}
+/**
+ * Gets the user input todo item from the form.
+ */
+function getItem():ToDoItem{
+    let title = (<HTMLInputElement> document.getElementById('title')).value; 
+    let item = new ToDoItem(title);
+
+    let deadline = (<HTMLInputElement> document.getElementById('deadline')).value;
+    item.deadline = new Date(deadline);
+}
+/**
+ * Displays ToDoItem on the page
+ * @param item 
+ */
+function displayToDoItem(item:ToDoItem):void {
+    let div = document.createElement('div');
+    div.innerHTML = '<input type="checkbox">' + item.title;
+    let displayDiv = document.getElementById('todo');
+    displayDiv.appendChild(div);
 }
